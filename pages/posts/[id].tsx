d611af0517696172ca4b5ee6ff953231dd6036db
An Yonghee <ayh0429@gmail.com>
2023-01-20 16:13:44 +0900

    add id.tsx

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,48 @@
+import { getAllPostIds } from "@/posts/lib/post";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
+import postStyles from "@/styles/Post.module.css";
+import homeStyles from "@/styles/Home.module.css";
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div className={homeStyles.container}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={homeStyles.headingX1}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // {id: 'pre-rendering}
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
